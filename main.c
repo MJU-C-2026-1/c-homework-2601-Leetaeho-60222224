074d644b37b39a209e8c88d51f327412c1b71ac0
Leetaeho-60222224 <andrew5500@mju.ac.kr>
2026-04-03 14:25:52 +0900

    Update main.c

--- main.c
+++ main.c
@@ -43,7 +43,7 @@ int main()
   printf("\n=== 카테고리 '%c'의 칼로리 계산 결과===\n", category_code );
   printf("- 탄수화물 %dg : %dg kcal\n", crab_grams, crab_grams * 4);
   printf("- 단백질 %dg : %dg kcal\n", protein_grams, protein_grams * 4);
-  printf("- 지방 %.lfg : %lfg kcal\n", fat_grams, fat_grams * 9.0);
+  printf("- 지방 %.1fg : %.lfg kcal\n", fat_grams, fat_grams * 9.0);
   printf("--------------------------------------------------------------\n");
   printf("▶ 총 섭취 칼로리 : %.lf kcal\n", total_calories);
   printf("==============================================================\n");
